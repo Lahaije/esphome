d68d281a8bd0552d748b5a833347fa075a3629b2
Lahaije <62953448+Lahaije@users.noreply.github.com>
2024-05-06 19:28:57 +0200

    Add files via upload

---
+++ components/regenmeter/regenmeter.cpp
@@ -0,0 +1,23 @@
+#include "esphome/core/log.h"
+#include "regenmeter.h"
+
+namespace esphome {
+  namespace regenmeter {
+
+    static const char *TAG = "regenmeter.output";
+
+    void RegenOutput::setup(){
+
+    }
+
+    void RegenOutput::write_state(float state){
+
+    }
+
+    void RegenOutput::dump_config() {
+        ESP_LOGCONFIG(TAG, "Empty custom float output");
+    }
+
+    } //namespace regenmeter
+} //namespace esphome
+
