f4bdef2b1f3b960075be39efaec4e89789045f68
Lahaije <62953448+Lahaije@users.noreply.github.com>
2024-05-06 19:31:33 +0200

    Add files via upload

--- components/regenmeter/output.py
+++ components/regenmeter/output.py
@@ -3,12 +3,12 @@ import esphome.config_validation as cv
 from esphome.components import output
 from esphome.const import CONF_ID
 
-empty_float_output_ns = cg.esphome_ns.namespace('empty_float_output')
-EmptyFloatOutput = empty_float_output_ns.class_('EmptyFloatOutput', output.FloatOutput,
+regenmeter_output_ns = cg.esphome_ns.namespace('regenmeter')
+RegenOutput = regenmeter_output_ns.class_('RegenOutput', output.FloatOutput,
                                   cg.Component)
 
 CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
-    cv.Required(CONF_ID): cv.declare_id(EmptyFloatOutput),
+    cv.Required(CONF_ID): cv.declare_id(RegenOutput),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
